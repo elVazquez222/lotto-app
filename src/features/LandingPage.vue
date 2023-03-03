a1fceede681d3d3115839c5b0b197536a74c26f1
jose.guerrerovazquez <joseguerrerovazquez@bundeswehr.org>
2023-03-03 19:00:22 +0100

    setup basic app structure

---
+++ src/features/LandingPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="landingPage">
+    <h1>Welcome to Lotto Madness</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LandingPage",
+  setup() {
+    return { };
+  },
+  components: {},
+};
+</script>
+
+<style scoped>
+  .landingPage {
+
+  }
+</style>
---
+++ src/features/PowerBall.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="powerBall">
+    <h1>Play PowerBall</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PowerBall",
+  setup() {
+    return { };
+  },
+  components: {},
+};
+</script>
+
+<style scoped>
+  .powerBall {
+
+  }
+</style>
