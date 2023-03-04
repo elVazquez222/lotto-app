17391e4f745db29a4eed93f5f64269c6b526b16a
jose.guerrerovazquez <joseguerrerovazquez@bundeswehr.org>
2023-03-04 18:25:19 +0100

    create PowerBallDrawingElement component

---
+++ src/features/powerBall/components/PowerBallDrawingElement.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="powerBallDrawingElement">
+    <h1>{{drawing.prettyDrawDate}} </h1>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "PowerBallDrawingElement",
+  props: ["drawing"],
+  components: {},
+  setup() {
+    return { };
+  },
+};
+</script>
+
+<style scoped>
+  .powerBallDrawingElement {
+
+  }
+</style>
