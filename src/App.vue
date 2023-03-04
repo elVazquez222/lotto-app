074f420347e42b38128685b4aa5ef282b11804f7
jose.guerrerovazquez <joseguerrerovazquez@bundeswehr.org>
2023-03-04 10:41:05 +0100

    add footer

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/power-ball">Power Ball</router-link>
+    <router-link to="/">Start</router-link> |
+    <router-link to="/power-ball">Power Ball ✨</router-link>
   </nav>
   <router-view/>
+  <footer>
+    <div class="legals touchable impressum">Impressum</div>
+    <div class="legals touchable agb">AGB</div>
+  </footer>
 </template>
 
+
 <style>
+
+
+:root {
+  --global-spacing-s: 8px;
+  --global-spacing-m: 16px;
+  --global-spacing-m: 32px;
+}
+
+body {
+  margin: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.touchable {
+  cursor: pointer;
 }
 
 nav {
@@ -26,5 +45,19 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+footer {
+  display: flex;
+  gap: 1rem;
+  justify-content: center;
+
+  margin-top: auto;
+  padding: var(--global-spacing-s);
+  background: #272727;
+  color: white;
+}
+footer > div:hover {
+  color: aquamarine;
 }
 </style>
---
+++ src/components/sample.component.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="component">
+    <h1>Headline</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Component",
+  components: {},
+  setup() {
+    return { };
+  },
+};
+</script>
+
+<style scoped>
+  .component {
+
+  }
+</style>
