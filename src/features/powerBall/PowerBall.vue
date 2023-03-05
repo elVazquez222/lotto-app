1f088ff47419ac80eb4feccb9a8d68bea8697c56
jose.guerrerovazquez <joseguerrerovazquez@bundeswehr.org>
2023-03-05 16:55:55 +0100

    load different chunks from EP

--- src/features/powerBall/PowerBall.vue
+++ src/features/powerBall/PowerBall.vue
@@ -1,10 +1,21 @@
 <template>
   <div class="powerBall">
-    <h1>PowerBall Drawings: </h1>
+    <header>
+      <h1>PowerBall Drawings: </h1>
+      <div v-if="drawingsWithFormattedDates.length !== 0" class="currentDateSpan">
+        <div>{{drawingsWithFormattedDates[0].shortenedDate}} - {{drawingsWithFormattedDates[drawingsWithFormattedDates.length-1].shortenedDate}}</div>
+        <div class="currentDateSpanNavigator">
+          <span :class="['touchable', currentPageIndex === 0 && 'inactive']" @click="setCurrentPageIndex(-1)">load newer</span>
+          <span class="touchable" @click="setCurrentPageIndex(1)">load older</span>
+        </div>
+      </div>
+    </header>
+
     <PowerBallDrawings
       v-if="hasDrawings"
       :drawings="drawingsWithFormattedDates"
     />
+
     <div v-else class="loading">loading</div>
   </div>
 </template>
@@ -23,15 +34,22 @@ export default {
   name: "PowerBall",
   components: {PowerBallDrawings},
   setup() {
+    const DRAWINGS_PER_PAGE = 10;
+    const currentPageIndex = ref(0);
+
     const drawings = ref<PowerBallDrawHistoryResponse | null>(null);
     const drawingsWithFormattedDates = ref<FinishedDrawPayloadWithFormattedDateFields[]>([]);
     const hasDrawings = ref(false);
 
-    onMounted(async() => {
-       drawings.value = await fetchPowerBallDrawHistory();
-       addShortDateInfoToDrawings();
-       hasDrawings.value = drawingsWithFormattedDates.value.length !== 0;
+    onMounted(() => {
+      fetchDrawings();
     })
+
+    const fetchDrawings = async() => {
+      drawings.value = await fetchPowerBallDrawHistory(DRAWINGS_PER_PAGE, currentPageIndex.value * DRAWINGS_PER_PAGE);
+      addShortDateInfoToDrawings();
+      hasDrawings.value = drawingsWithFormattedDates.value.length !== 0;
+    }
 
     const addShortDateInfoToDrawings = () => {
 
@@ -46,9 +64,18 @@ export default {
       })
     }
 
+    const setCurrentPageIndex = (direction: -1|1) => {
+      currentPageIndex.value = currentPageIndex.value += direction;
+      fetchDrawings();
+      console.log(currentPageIndex.value);
+
+    }
+
     return {
+      currentPageIndex,
       hasDrawings,
-      drawingsWithFormattedDates
+      drawingsWithFormattedDates,
+      setCurrentPageIndex
      };
   },
 };
@@ -58,6 +85,33 @@ export default {
   h1 {
     text-align: center;
     padding: var(--global-spacing-l);
+  }
+
+  header {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    margin-bottom: var(--global-spacing-m);
+    font-size: 1rem;
+    width: 100%;
+  }
+
+  .currentDateSpanNavigator {
+    display: flex;
+    gap: var(--global-spacing-xl);
+    margin: var(--global-spacing-m) 0;
+  }
+  .currentDateSpanNavigator span {
+    font-size: 12px;
+    color: blue;
+    text-decoration: underline;
+  }
+  .currentDateSpanNavigator .inactive {
+    cursor: not-allowed;
+    color: grey;
+    text-decoration: none;
   }
 
   .powerBall {
